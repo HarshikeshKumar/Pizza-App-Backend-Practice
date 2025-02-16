e8cab4a8f3e3942b6d3e89f1f90697243e66a67f
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-02-16 19:38:43 +0530

    Final Express Starter Code

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,6 +1,12 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+// Here we are exporting all the env variables that the project uses
 module.exports = {
   PORT: process.env.PORT,
+  DB_URL: process.env.DB_URL,
 };
+
+// lHkYAuHjL36zpFMR
+
+// harshikeshkoili214
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
+// const bodyParser = require("body-parser");
 
 const ServerConfig = require("./config/serverConfig.js");
+const connectDB = require("./config/dbConfig.js");
 
 const app = express();
 
-app.listen(ServerConfig.PORT, () => {
+// app.use(bodyParser.json());
+// app.use(bodyParser.text());
+// app.use(bodyParser.urlencoded());
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/ping", (req, res) => {
+  console.log(req.body);
+  return res.json({
+    message: "Pong",
+  });
+});
+
+app.listen(ServerConfig.PORT, async () => {
+  await connectDB();
   console.log(`Server is running on ${ServerConfig.PORT}...!!`);
 });
 
